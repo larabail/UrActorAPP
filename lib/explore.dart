7a9d0c2db591967a9a3fea6735a8f6d9bf7a24e8
Lara Bailen <85242173+larabail@users.noreply.github.com>
2023-07-30 13:02:21 -0700

    minor change

--- lib/explore.dart
+++ lib/explore.dart
@@ -55,7 +55,7 @@ class Explore extends StatelessWidget {
           height: 54,
         )),
       ),
-      body: Text("Explore Page"),
+      body: Text("Explore Page Here"),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.grey,
         unselectedItemColor: Colors.grey,
