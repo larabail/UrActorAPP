32ca3d840084c74e3811f69b1fb457d8a15f693d
Lara Bailen <85242173+larabail@users.noreply.github.com>
2023-07-31 22:28:59 -0700

    added playlist joining and creating

--- lib/playlists.dart
+++ lib/playlists.dart
@@ -4,9 +4,38 @@ import 'package:uractor/search.dart';
 import 'package:uractor/main.dart';
 import 'package:uractor/list_result.dart';
 
+import 'list_add_popup.dart';
+import 'list_join_popup.dart';
+
 Map list_result = {};
 
-class Playlists extends StatelessWidget {
+class Playlists extends StatefulWidget {
+  @override
+  _PlaylistsState createState() => _PlaylistsState();
+}
+
+class _PlaylistsState extends State<Playlists> {
+  bool isJoinListPanelOpen = false;
+  bool isAddListPanelOpen = false;
+
+  void _toggleJoinListPanel() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return ListJoinDialogue();
+      },
+    );
+  }
+
+  void _toggleAddListPanel() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return ListAddDialogue();
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     int _selectedIndex = 0;
@@ -16,7 +45,7 @@ class Playlists extends StatelessWidget {
     dynamic accessCode = "";
 
     final List<Widget> _pages = [
-      MyApp(),
+      const MyApp(),
       Search(),
       Playlists(),
       Profile(),
@@ -34,98 +63,137 @@ class Playlists extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFF121212),
       appBar: AppBar(
-        backgroundColor: Color(0xFF121212),
+        backgroundColor: const Color(0xFF121212),
         title: Center(
             child: Image.asset(
           'assets/logo.png',
           height: 54,
         )),
       ),
-      body: Center(
-        child: ListView.builder(
-          itemCount: playlists.length,
-          itemBuilder: (context, index) {
-            String key = playlists.keys.elementAt(index);
-            dynamic value = playlists[key]['Name'];
-            dynamic image = playlists[key]['CoverPhoto'];
-            dynamic movies = playlists[key]['Movies'];
-            dynamic tvshows = playlists[key]['TV Shows'];
-            dynamic accessCode = playlists[key]['AccessCode'];
-            return GestureDetector(
-              onTap: () {
-                // Handle the click event here
-                list_result["Movies"] = movies;
-                list_result["TVShows"] = tvshows;
-                list_result["Backdrop"] = image;
-                list_result["Name"] = value;
-                list_result["AccessCode"] = accessCode;
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ListResult()),
-                );
-              },
-              child: Container(
-                margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0),
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(27),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    _toggleJoinListPanel();
+                  },
+                  icon: const Icon(Icons.playlist_add_check),
+                  label: const Text('Join Existing List'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors
+                        .green, // Change the background color of the button
+                    // Change the text color of the button
+                  ),
                 ),
-                child: Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            image,
-                          ),
-                          fit: BoxFit.cover,
+                ElevatedButton.icon(
+                  onPressed: () {
+                    _toggleAddListPanel();
+                  },
+                  icon: const Icon(Icons.add),
+                  label: const Text('Add New List'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        Colors.red, // Change the background color of the button
+                    // Change the text color of the button
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.75,
+              child: Center(
+                child: ListView.builder(
+                  itemCount: playlists.length,
+                  itemBuilder: (context, index) {
+                    String key = playlists.keys.elementAt(index);
+                    dynamic value = playlists[key]['Name'];
+                    dynamic image = playlists[key]['CoverPhoto'];
+                    dynamic movies = playlists[key]['Movies'];
+                    dynamic tvshows = playlists[key]['TV Shows'];
+                    dynamic accessCode = playlists[key]['AccessCode'];
+                    return GestureDetector(
+                      onTap: () {
+                        // Handle the click event here
+                        list_result["Movies"] = movies;
+                        list_result["TVShows"] = tvshows;
+                        list_result["Backdrop"] = image;
+                        list_result["Name"] = value;
+                        list_result["AccessCode"] = accessCode;
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => ListResult()),
+                        );
+                      },
+                      child: Container(
+                        margin:
+                            const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 5.0),
+                        height: 200,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(27),
                         ),
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                            Colors.transparent,
-                            Colors.black.withOpacity(1),
+                        child: Stack(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    image,
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                gradient: LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [
+                                    Colors.transparent,
+                                    Colors.black.withOpacity(1),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Align(
+                                alignment: Alignment.bottomRight,
+                                child: Text(
+                                  value,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    letterSpacing: 1.5,
+                                    wordSpacing: 2,
+                                    height: 1.5,
+                                  ),
+                                ),
+                              ),
+                            ),
                           ],
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Align(
-                        alignment: Alignment.bottomRight,
-                        child: Text(
-                          value,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 1.5,
-                            wordSpacing: 2,
-                            height: 1.5,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+                    );
+                  },
                 ),
               ),
-            );
-          },
+            ),
+          ],
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.blue,
         unselectedItemColor: Colors.grey,
         type: BottomNavigationBarType.fixed,
-        backgroundColor: Color(0xFF121212),
+        backgroundColor: const Color(0xFF121212),
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
