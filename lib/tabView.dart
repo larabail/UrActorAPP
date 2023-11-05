5f3d2b1d4869340a7aa4517d3d48c72e5cef66be
Lara Bailen <85242173+larabail@users.noreply.github.com>
2023-11-05 11:08:32 -0500

    made fav watchlist and seen have common tabView widget

---
+++ lib/tabView.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+import 'movie_result.dart';
+import 'tvshow_result.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class MyTabView extends StatelessWidget {
+  final List<dynamic> favItems;
+
+  MyTabView({required this.favItems});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: (favItems.reversed.toList().length / 3).ceil(),
+      itemBuilder: (context, index) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: List.generate(3, (i) {
+            final itemIndex = index * 3 + i;
+            if (itemIndex < favItems.reversed.toList().length) {
+              final item = favItems.reversed.toList()[itemIndex];
+              return ItemCard(item: item);
+            }
+            return SizedBox.shrink(); // Return an empty widget if no item
+          }),
+        );
+      },
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  final List<dynamic> item;
+
+  ItemCard({required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Map<String, dynamic>>(
+      future: getData(item[1], item[0]),
+      builder: (BuildContext context, AsyncSnapshot<Map> snapshot) {
+        if (snapshot.hasData) {
+          return GestureDetector(
+            onTap: () {
+              // Handle the click event here
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => snapshot.data!['type'] == "Movies"
+                      ? MovieResult()
+                      : TVShowResult(),
+                ),
+              );
+            },
+            child: Container(
+              margin: const EdgeInsets.fromLTRB(5.0, 10.0, 10.0, 0),
+              width: MediaQuery.of(context).size.width * 0.28,
+              height: MediaQuery.of(context).size.height * 0.18,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(27),
+                image: DecorationImage(
+                  image: NetworkImage(snapshot.data!['poster']),
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return const Center(child: Text("Failed to load movie details"));
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
+
+final String api_key_actor = "?api_key=700cd4fab994df56eb41b34d38c4762a";
+final String imgLink = 'https://image.tmdb.org/t/p/w500/';
+String link = "https://api.themoviedb.org/3/movie/";
+List<Map<String, dynamic>> movies = [];
+
+Future<Map<String, dynamic>> getData(id, type) async {
+  Map<String, dynamic> data = {};
+  if (type == "TVShows") {
+    link = 'https://api.themoviedb.org/3/tv/';
+  } else {
+    link = 'https://api.themoviedb.org/3/movie/';
+  }
+  final response = await http.get(Uri.parse('$link$id$api_key_actor'));
+  if (response.statusCode == 200) {
+    final json = jsonDecode(response.body);
+    if (type == "TVShows") {
+      data['title'] = json['name'];
+    } else {
+      data['title'] = json['title'];
+    }
+    if (json['poster_path'] == null) {
+      data['poster'] = 'assets/question_mark.png';
+    } else {
+      data['poster'] = imgLink + json['poster_path'];
+    }
+    data['id'] = json['id'];
+    data['type'] = type;
+    if (!containsMap(movies, data)) {
+      movies.add(data);
+    }
+  } else {
+    throw Exception('Failed to load movie details');
+  }
+  return data;
+}
+
+bool containsMap(List<Map<String, dynamic>> list, Map<String, dynamic> map) {
+  String jsonString = json.encode(map);
+  for (int i = 0; i < list.length; i++) {
+    if (json.encode(list[i]) == jsonString) {
+      return true;
+    }
+  }
+  return false;
+}
