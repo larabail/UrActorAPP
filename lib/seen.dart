8d1e4494e3ab0678496dd1f6dd08fbcb0a5b430a
Lara Bailen <85242173+larabail@users.noreply.github.com>
2023-11-24 12:25:48 -0500

    moved files

--- lib/seen.dart
+++ lib/seen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'common/appbar.dart';
 import 'common/bottom_app_bar.dart';
 import 'main.dart';
-import 'tabView.dart';
+import 'common/tabView.dart';
 
 class Seen extends StatelessWidget {
   @override
